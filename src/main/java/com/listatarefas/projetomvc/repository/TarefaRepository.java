c2ff93bb7f063fd7ec7602d37efd253dd3b55562
Jaison Staloch Junior <staloch.dev@gmail.com>
2025-10-14 20:13:14 -0300

    Feat: adiciona o repositório JPA para a entidade Tarefa

---
+++ src/main/java/com/listatarefas/projetomvc/repository/TarefaRepository.java
@@ -0,0 +1,12 @@
+package com.listatarefas.projetomvc.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.listatarefas.projetomvc.model.Tarefa;
+
+@Repository
+public class TarefaRepository extends JpaRepository <Tarefa, Long> {
+
+
+    
+}
