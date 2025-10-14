a019a1dc93b99fe88ec0ac1293846718cc4d40ca
Jaison Staloch Junior <staloch.dev@gmail.com>
2025-10-14 19:41:23 -0300

    Feat: desenvolve a entidade Tarefa e a estrutura inicial de pacotes

---
+++ src/main/java/com/listatarefas/projetomvc/model/Tarefa.java
@@ -0,0 +1,66 @@
+package com.listatarefas.projetomvc.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Tarefa {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String titulo;
+    private String descricao;
+    private boolean concluida;
+    private LocalDate dataCriacao;
+
+    public Tarefa() {
+        this.dataCriacao = LocalDate.now();
+        this.concluida   = false;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isConcluida() {
+        return concluida;
+    }
+
+    public void setConcluida(boolean concluida) {
+        this.concluida = concluida;
+    }
+
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }  
+}
